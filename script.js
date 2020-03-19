9c77feb67a7be00644d487104b80a7e898b2d6e4
OK <oleg.kokoshinskii@gmail.com>
2020-03-19 14:01:17 +1000

    Небольшой рефакторинг + комментарии.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+//Формируем список городов
 const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Калиниград', 'Владивосток', 'Нижний Новгород', 'Сочи', 'Крым', 'Самара', 'Волгоград', 'Екатеринбург'];
 
+//Получаем элементы разметки
 const formSearch = document.querySelector('.form-search'),
       inputCitiesFrom = document.querySelector('.input__cities-from'),
       inputCitiesTo = document.querySelector('.input__cities-to'),
       dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
       dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
       inputDateDepart = document.querySelector('.input__date-depart');
-
+//Фильтр городов в зависимости от ввода пользователя
 const showCity = (input, list) => {
   list.textContent = '';
-
+    //Проверка, пустой ли инпут, если да, функция не выполняется
    if (input.value === '') {
     return;
    }
@@ -17,7 +19,7 @@ const showCity = (input, list) => {
     const fixItem = item.toLowerCase();
     return fixItem.includes(input.value.toLowerCase());
   });
-
+//Добавляем элементы li в .dropdown__city
   filterCity.forEach((item) => {
     const li = document.createElement('li');
     li.classList.add('dropdown__city');
@@ -26,26 +28,26 @@ const showCity = (input, list) => {
   });
 };
 
-inputCitiesFrom.addEventListener('input', () => {
-  showCity(inputCitiesFrom, dropdownCitiesFrom)
-});
-
-dropdownCitiesFrom.addEventListener('click', (event) => {
+const selectCity = (event, input, list) => {
   const target = event.target;
   if(target.tagName.toLowerCase() === 'li') {
-    inputCitiesFrom.value = target.textContent;
-    dropdownCitiesFrom.textContent = '';
+    input.value = target.textContent;
+    list.textContent = '';
   }
+}
+//Показываем список городов соответственно вводу
+inputCitiesFrom.addEventListener('input', () => {
+  showCity(inputCitiesFrom, dropdownCitiesFrom)
 });
 
 inputCitiesTo.addEventListener('input', () => {
   showCity(inputCitiesTo, dropdownCitiesTo)
 });
+//Подставляем город в поле по клику
+dropdownCitiesFrom.addEventListener('click', (event) => {
+  selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
+});
 
 dropdownCitiesTo.addEventListener('click', (event) => {
-  const target = event.target;
-  if(target.tagName.toLowerCase() === 'li') {
-    inputCitiesTo.value = target.textContent;
-    dropdownCitiesTo.textContent = '';
-  }
+  selectCity(event, inputCitiesTo, dropdownCitiesTo);
 });
