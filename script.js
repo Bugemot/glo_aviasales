28805fa451e67271b995aec5486f93cc2295e0bb
OK <oleg.kokoshinskii@gmail.com>
2020-03-20 16:06:14 +1000

    Вторая домашка

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-//Формируем список городов
-const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Калиниград', 'Владивосток', 'Нижний Новгород', 'Сочи', 'Крым', 'Самара', 'Волгоград', 'Екатеринбург'];
+//Данные
+const CITY_API = 'database/cities.json',
+      PROXY = 'https://cors-anywhere.herokuapp.com/',
+      API_KEY='59b2326ef4e36e05010ba16e70acb7fe',
+      CALENDAR = 'http://min-prices.aviasales.ru/calendar_preload';
+let city = [];
 
+//Переменные
 //Получаем элементы разметки
 const formSearch = document.querySelector('.form-search'),
       inputCitiesFrom = document.querySelector('.input__cities-from'),
@@ -8,6 +13,26 @@ const formSearch = document.querySelector('.form-search'),
       dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
       dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
       inputDateDepart = document.querySelector('.input__date-depart');
+
+//Функции
+const getData = (url, callback) => {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', url);
+
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState !== 4) return;
+
+    if (request.status === 200) {
+       callback(request.response); 
+    } else {
+      console.error(request.status);
+    }
+  });
+
+  request.send();
+}
+
 //Фильтр городов в зависимости от ввода пользователя
 const showCity = (input, list) => {
   list.textContent = '';
@@ -16,14 +41,15 @@ const showCity = (input, list) => {
     return;
    }
   const filterCity = city.filter((item) => {
-    const fixItem = item.toLowerCase();
+    const fixItem = item.name.toLowerCase();
     return fixItem.includes(input.value.toLowerCase());
   });
-//Добавляем элементы li в .dropdown__city
+
+  //Добавляем элементы li в .dropdown__city
   filterCity.forEach((item) => {
     const li = document.createElement('li');
     li.classList.add('dropdown__city');
-    li.textContent = item;
+    li.textContent = item.name;
     list.append(li);
   });
 };
@@ -35,14 +61,17 @@ const selectCity = (event, input, list) => {
     list.textContent = '';
   }
 }
+
+//Обработчики событий
 //Показываем список городов соответственно вводу
 inputCitiesFrom.addEventListener('input', () => {
   showCity(inputCitiesFrom, dropdownCitiesFrom)
 });
 
 inputCitiesTo.addEventListener('input', () => {
-  showCity(inputCitiesTo, dropdownCitiesTo)
+  showCity(inputCitiesTo, dropdownCitiesTo);
 });
+
 //Подставляем город в поле по клику
 dropdownCitiesFrom.addEventListener('click', (event) => {
   selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
@@ -51,3 +80,10 @@ dropdownCitiesFrom.addEventListener('click', (event) => {
 dropdownCitiesTo.addEventListener('click', (event) => {
   selectCity(event, inputCitiesTo, dropdownCitiesTo);
 });
+
+//Вызовы функций
+getData(CITY_API, (data) => {
+  city = JSON.parse(data).filter(item => item.name)
+});
+
+
